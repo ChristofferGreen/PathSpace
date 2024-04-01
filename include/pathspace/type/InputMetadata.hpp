04df526db0696c261082ff9dfd68b72bff1a8de4
Christoffer Green <christoffer.green@gmail.com>
2024-04-01 18:04:09 +0000

    Add alpaca serialization

--- include/pathspace/type/InputMetadata.hpp
+++ include/pathspace/type/InputMetadata.hpp
@@ -8,7 +8,7 @@
 #include "pathspace/utils/ByteQueue.hpp"
 #include "pathspace/utils/ByteQueueSerializer.hpp"
 
-#include "glaze/glaze.hpp"
+#include <alpaca/alpaca.h>
 
 #include <cereal/types/string.hpp>
 #include <cereal/types/variant.hpp>
@@ -24,8 +24,52 @@ TODO
 --------------
 * Add a deserialize_read that can deserialize a single item
 * Change queue to deque for serialized data so we can have random access, perhaps find internet datastructure that is better
-* Add tests that const deserializes multiple objects making sure the data doesnt change 
+* Add tests that const deserializes multipßle objects making sure the data doesnt change 
 */
+
+template<typename T>
+concept AlpacaCompatible = !std::is_same_v<T, std::function<void()>> && 
+                                        !std::is_same_v<T, void(*)()> &&
+                                        !std::is_pointer<T>::value &&
+                                        requires(T t, std::vector<uint8_t>& v) {
+    { serialize_alpaca<T>(&t, v) };
+    { deserialize_alpaca<T>(&t, v) };
+};
+
+template<typename T>
+static auto serialize_alpaca(void const *objPtr, std::vector<uint8_t> &bytes) -> void {
+    T const &obj = *static_cast<T const *>(objPtr);
+    alpaca::serialize(obj, bytes);
+}
+
+template<typename T>
+static auto deserialize_alpaca(void *objPtr, std::vector<uint8_t> &bytes) -> void {
+    T &obj = *static_cast<T*>(objPtr);
+    std::error_code ec;
+    alpaca::deserialize<T>(obj, bytes, ec);
+}
+
+template<typename T>
+static auto serialize_fundamental(void const *objPtr, std::vector<uint8_t> &bytes) -> void {
+    static_assert(std::is_fundamental_v<T>, "T must be a fundamental type");
+    T const &obj = *static_cast<T const *>(objPtr);
+    auto const *begin = reinterpret_cast<uint8_t const *>(&obj);
+    auto const *end = begin + sizeof(T);
+    bytes.insert(bytes.end(), begin, end);
+}
+
+template<typename T>
+static auto deserialize_fundamental(void *objPtr, std::vector<uint8_t> &bytes) -> void {
+    static_assert(std::is_fundamental_v<T>, "T must be a fundamental type");
+    if (bytes.size() < sizeof(T)) {
+        return;
+    }
+    T* obj = static_cast<T*>(objPtr);
+    std::copy(bytes.begin(), bytes.begin() + sizeof(T), reinterpret_cast<uint8_t*>(obj));
+    bytes.erase(bytes.begin(), bytes.begin() + sizeof(T));
+}
+
+// -----
 
 template<typename T>
 static auto serialize(void const *objPtr, ByteQueue &byteQueue) -> void {
@@ -71,10 +115,32 @@ struct InputMetadataT {
             }
         }();
 
+    static constexpr auto serializationFuncPtr2 = 
+        []() {
+            if constexpr (std::is_fundamental<T>::value) {
+                return &serialize_fundamental<T>;
+            } else if constexpr (AlpacaCompatible<T>) {
+                return &serialize_alpaca<T>;
+            } else {
+                return nullptr;
+            }
+        }();
+
     static constexpr auto deserializationFuncPtr = 
         []() -> std::conditional_t<SerializableAndDeserializable<T>, decltype(&deserialize<T>), std::nullptr_t> {
             if constexpr (SerializableAndDeserializable<T>) {
                 return &deserialize<T>;
+            } else {
+                return nullptr;
+            }
+        }();
+
+    static constexpr auto deserializationFuncPtr2 = 
+        []() {
+            if constexpr (std::is_fundamental<T>::value) {
+                return &deserialize_fundamental<T>;
+            } else if constexpr (AlpacaCompatible<T>) {
+                return &deserialize_alpaca<T>;
             } else {
                 return nullptr;
             }
@@ -88,6 +154,17 @@ struct InputMetadataT {
                 return nullptr;
             }
         }();
+
+    static constexpr auto deserializationFuncPtrConst2 = 
+        []() {
+            if constexpr (std::is_fundamental<T>::value) {
+                return &deserialize_fundamental<T>;
+            } else if constexpr (AlpacaCompatible<T>) {
+                return &deserialize_alpaca<T>;
+            } else {
+                return nullptr;
+            }
+        }();
 };
 
 struct InputMetadata {
@@ -96,13 +173,19 @@ struct InputMetadata {
     InputMetadata(InputMetadataT<CVRefT> const &obj)
         : id(&typeid(T)),
           serializationFuncPtr(obj.serializationFuncPtr),
+          serializationFuncPtr2(obj.serializationFuncPtr2),
           deserializationFuncPtr(obj.deserializationFuncPtr),
-          deserializationFuncPtrConst(obj.deserializationFuncPtrConst)
+          deserializationFuncPtr2(obj.deserializationFuncPtr2),
+          deserializationFuncPtrConst(obj.deserializationFuncPtrConst),
+          deserializationFuncPtrConst2(obj.deserializationFuncPtrConst2)
           {}
     std::type_info const *id = nullptr;
     void (*serializationFuncPtr)(void const *obj, ByteQueue&) = nullptr;
+    void (*serializationFuncPtr2)(void const *obj, std::vector<uint8_t>&) = nullptr;
     void (*deserializationFuncPtr)(void *obj, ByteQueue&) = nullptr;
+    void (*deserializationFuncPtr2)(void *obj, std::vector<uint8_t>&) = nullptr;
     void (*deserializationFuncPtrConst)(void *obj, ByteQueue const&) = nullptr;
+    void (*deserializationFuncPtrConst2)(void *obj, std::vector<uint8_t>&) = nullptr;
 };
 
 } // namespace SP
---
+++ tests/unit/ext/test_alpaca.cpp
@@ -0,0 +1,28 @@
+#include "ext/doctest.h"
+
+#include <alpaca/alpaca.h>
+
+struct Person {
+  std::string name;
+  uint16_t age;
+  std::vector<std::string> aliases;
+
+  bool operator==(Person const &other) const = default;
+
+  template<class T>
+  void pack(T &pack) {
+    pack(name, age, aliases);
+  }
+};
+
+TEST_CASE("Alpaca") {
+    auto const person = Person{"John", 22, {"Ripper", "Silverhand"}};
+    std::vector<uint8_t> bytes;
+    SUBCASE("Simple struct") {
+        alpaca::serialize(person, bytes);
+        std::error_code ec;
+        auto john = alpaca::deserialize<Person>(bytes, ec);
+        REQUIRE_FALSE(ec);
+        REQUIRE(john==person);
+    }
+}
