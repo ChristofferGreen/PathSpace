460e0f3b3daa6db41f640ccadba595ac2fec8849
Christoffer Green <christoffer.green@gmail.com>
2025-01-21 21:28:23 +0100

    Move read and take methods from PathSpace to PathSpaceBase for improved code organization and reusability

--- src/pathspace/PathSpace.hpp
+++ src/pathspace/PathSpace.hpp
@@ -25,58 +25,6 @@ public:
     explicit PathSpace(TaskPool* pool = nullptr);
     ~PathSpace();
 
-    /**
-     * @brief Reads data from the PathSpace at the specified path.
-     *
-     * @tparam DataType The type of data to be read.
-     * @param path The concrete path from which to read the data.
-     * @param options Options controlling the read behavior, such as blocking policies.
-     * @return Expected<DataType> containing the read data if successful, or an error if not.
-     */
-    template <typename DataType, StringConvertible S>
-    auto read(S const& pathIn, Out const& options = {}) const -> Expected<DataType> {
-        sp_log("PathSpace::read", "Function Called");
-        Iterator const path{pathIn};
-        if (auto error = path.validate(options.validationLevel))
-            return std::unexpected(*error);
-        DataType obj;
-        if (auto error = const_cast<PathSpace*>(this)->out(path, InputMetadataT<DataType>{}, options, &obj))
-            return std::unexpected{*error};
-        return obj;
-    }
-    template <FixedString pathIn, typename DataType>
-        requires(validate_path(pathIn))
-    auto read(Out const& options = {}) const -> Expected<DataType> {
-        sp_log("PathSpace::read", "Function Called");
-        return this->read<DataType>(pathIn, options & OutNoValidation{});
-    }
-
-    /**
-     * @brief Reads and removes data from the PathSpace at the specified path.
-     *
-     * @tparam DataType The type of data to be extractbed.
-     * @param path The concrete path from which to extract the data.
-     * @return Expected<DataType> containing the extractbed data if successful, or an error if not.
-     */
-    template <typename DataType, StringConvertible S>
-    auto take(S const& pathIn, Out const& options = {}) -> Expected<DataType> {
-        sp_log("PathSpace::extract", "Function Called");
-        Iterator const path{pathIn};
-        if (auto error = path.validate(options.validationLevel))
-            return std::unexpected(*error);
-        DataType obj;
-        if (auto error = this->out(path, InputMetadataT<DataType>{}, options & Pop{}, &obj))
-            return std::unexpected(*error);
-        return obj;
-    }
-
-    template <FixedString pathIn, typename DataType>
-        requires(validate_path(pathIn))
-    auto take(Out const& options = {}) -> Expected<DataType> {
-        sp_log("PathSpace::extract", "Function Called");
-        return this->take<DataType>(pathIn, options & Pop{} & OutNoValidation{});
-    }
-
     virtual auto clear() -> void;
 
 protected:
--- src/pathspace/PathSpaceBase.hpp
+++ src/pathspace/PathSpaceBase.hpp
@@ -2,6 +2,7 @@
 #include "core/Error.hpp"
 #include "core/In.hpp"
 #include "core/InsertReturn.hpp"
+#include "core/Out.hpp"
 #include "log/TaggedLogger.hpp"
 #include "path/Iterator.hpp"
 #include "path/utils.hpp"
@@ -45,6 +46,58 @@ public:
     auto insert(DataType&& data, In const& options = {}) -> InsertReturn {
         sp_log("PathSpace::insert", "Function Called");
         return this->insert(pathIn, std::forward<DataType>(data), options & InNoValidation{});
+    }
+
+    /**
+     * @brief Reads data from the PathSpace at the specified path.
+     *
+     * @tparam DataType The type of data to be read.
+     * @param path The concrete path from which to read the data.
+     * @param options Options controlling the read behavior, such as blocking policies.
+     * @return Expected<DataType> containing the read data if successful, or an error if not.
+     */
+    template <typename DataType, StringConvertible S>
+    auto read(S const& pathIn, Out const& options = {}) const -> Expected<DataType> {
+        sp_log("PathSpace::read", "Function Called");
+        Iterator const path{pathIn};
+        if (auto error = path.validate(options.validationLevel))
+            return std::unexpected(*error);
+        DataType obj;
+        if (auto error = const_cast<PathSpaceBase*>(this)->out(path, InputMetadataT<DataType>{}, options, &obj))
+            return std::unexpected{*error};
+        return obj;
+    }
+    template <FixedString pathIn, typename DataType>
+        requires(validate_path(pathIn))
+    auto read(Out const& options = {}) const -> Expected<DataType> {
+        sp_log("PathSpace::read", "Function Called");
+        return this->read<DataType>(pathIn, options & OutNoValidation{});
+    }
+
+    /**
+     * @brief Reads and removes data from the PathSpace at the specified path.
+     *
+     * @tparam DataType The type of data to be extractbed.
+     * @param path The concrete path from which to extract the data.
+     * @return Expected<DataType> containing the extractbed data if successful, or an error if not.
+     */
+    template <typename DataType, StringConvertible S>
+    auto take(S const& pathIn, Out const& options = {}) -> Expected<DataType> {
+        sp_log("PathSpace::extract", "Function Called");
+        Iterator const path{pathIn};
+        if (auto error = path.validate(options.validationLevel))
+            return std::unexpected(*error);
+        DataType obj;
+        if (auto error = this->out(path, InputMetadataT<DataType>{}, options & Pop{}, &obj))
+            return std::unexpected(*error);
+        return obj;
+    }
+
+    template <FixedString pathIn, typename DataType>
+        requires(validate_path(pathIn))
+    auto take(Out const& options = {}) -> Expected<DataType> {
+        sp_log("PathSpace::extract", "Function Called");
+        return this->take<DataType>(pathIn, options & Pop{} & OutNoValidation{});
     }
 
 protected:
