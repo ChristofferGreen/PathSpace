58c6f16905513b06aa648350a730a2de30d487a0
Christoffer Green <christoffer.green@arm.com>
2026-02-14 23:43:27 +0100

    test(pathspace): cover span callback pointers

---
+++ tests/unit/pathspace/test_SpanCallbackPointers.cpp
@@ -0,0 +1,59 @@
+#include "third_party/doctest.h"
+
+#include <pathspace/PathSpace.hpp>
+
+#include <span>
+#include <vector>
+
+using namespace SP;
+
+namespace {
+std::vector<int>* gObserved = nullptr;
+
+void capture_ints(std::span<const int> ints) {
+    if (!gObserved) {
+        return;
+    }
+    gObserved->assign(ints.begin(), ints.end());
+}
+
+void increment_ints(std::span<int> ints) {
+    for (auto& value : ints) {
+        ++value;
+    }
+}
+} // namespace
+
+TEST_SUITE("pathspace.span.callback") {
+TEST_CASE("Span read accepts function pointer callbacks") {
+    PathSpace space;
+    CHECK(space.insert("/ints", 1).errors.empty());
+    CHECK(space.insert("/ints", 2).errors.empty());
+    CHECK(space.insert("/ints", 3).errors.empty());
+
+    std::vector<int> observed;
+    gObserved = &observed;
+    auto result = space.read("/ints", &capture_ints);
+    gObserved = nullptr;
+
+    REQUIRE(result.has_value());
+    CHECK(observed == std::vector<int>{1, 2, 3});
+}
+
+TEST_CASE("Span take accepts function pointer mutators") {
+    PathSpace space;
+    CHECK(space.insert("/ints", 4).errors.empty());
+    CHECK(space.insert("/ints", 5).errors.empty());
+
+    auto mutResult = space.take("/ints", &increment_ints);
+    REQUIRE(mutResult.has_value());
+
+    std::vector<int> observed;
+    auto readResult = space.read("/ints", [&](std::span<const int> ints) {
+        observed.assign(ints.begin(), ints.end());
+    });
+
+    REQUIRE(readResult.has_value());
+    CHECK(observed == std::vector<int>{5, 6});
+}
+}
